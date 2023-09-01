cfb618913fffdd4718781ce3a058e1e7bca3173c
ltzXiaoYanMo <2315049216@qq.com>
2023-09-01 20:50:07 +0800

    屎山终于有人维护了（？）

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import botpy
+from loguru import logger
 from botpy.message import Message
 
+import botpy
+import yaml
+import json
+import modules
+
+# 测试消息，发送官方文档
 class MyClient(botpy.Client):
     async def on_at_message_create(self, message: Message):
-        await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}")
+        await message.reply(content=f"机器人的官方文档:https://bot.ymbot.top")
---
+++ modules/zh_cn/channel.py
@@ -0,0 +1,11 @@
+import botpy
+from botpy.types.message import Message
+from botpy import logging
+
+class MyClient(botpy.Client):
+    async def on_at_message_create(self, message: Message):
+        await self.api.post_message(channel_id=message.channel_id, content="content")
+
+intents = botpy.Intents(public_guild_messages=True) 
+client = MyClient(intents=intents)
+client.run(appid={appid}, token={token})
