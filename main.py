f533578ae7b0adaba47c63a44a20831275867cac
ltzXiaoYanMo <2315049216@qq.com>
2023-09-11 21:20:38 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from botpy import logger
 from botpy.message import Message
 
 import botpy
-import yaml
 import json
+import pathlib
+
+# 读取json文件
+channel = json.loads(pathlib.Path("./json/chennel").read_text("mychannel"))
+channelid = json.loads(pathlib.Path("./json/chennel").read_text("boxid"))
 
 # 测试消息，发送官方文档
 class MyClient(botpy.Client):
