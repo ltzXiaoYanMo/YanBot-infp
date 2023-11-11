ba0e1bad9aa570ea3198cd0b0c82b3c1615282cf
ltzXiaoYanMo <xiajiaruimail@qq.com>
2023-11-11 13:34:43 +0800

    Dev:基本数据库配置

--- botfunc.py
+++ botfunc.py
@@ -1,8 +1,10 @@
 from botpy import logger
 
+import yaml
 import sys
 import json
 import portalocker
+
 
 def safe_file_read(filename: str, encode: str = "UTF-8", mode: str = "r") -> str or bytes:
     if mode == 'r':
@@ -18,6 +20,7 @@ def safe_file_read(filename: str, encode: str = "UTF-8", mode: str = "r") -> str
 
     return tmp
 
+
 def safe_file_write(filename: str, s, mode: str = "w", encode: str = "UTF-8"):
     if 'b' not in mode:
         with open(filename, mode, encoding=encode) as file:
@@ -29,6 +32,23 @@ def safe_file_write(filename: str, s, mode: str = "w", encode: str = "UTF-8"):
             portalocker.lock(file, portalocker.LOCK_EX)
             file.write(s)
             portalocker.unlock(file)
+
+
+try:
+    config_yaml = yaml.load(open('config.yaml', encoding='UTF-8'), Loader=yaml.FullLoader)
+except FileNotFoundError:
+    safe_file_write('config.yaml', """count_ban: 4  # 木鱼调用频率限制
+# 注意：腾讯云内容安全 API 收费为 0.0025/条
+text_review: false  # 是否使用腾讯云内容安全 API 对文本内容进行审核：true -> 是 | false -> 否，使用本地敏感词库
+# 请参考此文章就近设置地域：https://cloud.tencent.com/document/api/1124/51864#.E5.9C.B0.E5.9F.9F.E5.88.97.E8.A1.A8
+Region: ap-hongkong  # 使用香港地区 API
+enable_mysql: false  # 是否使用MySQL存储数据
+oneword: https://v1.hitokoto.cn/?c=f&encode=text # 一言api
+    """)
+    logger.error(
+        'config.yaml 文件不存在，已生成默认配置文件，请修改后重新运行。'
+    )
+    sys.exit(1)
 
 try:
     cloud_config_json = json.load(open('config.json', 'r', encoding='UTF-8'))
@@ -43,5 +63,21 @@ except FileNotFoundError:
   "Region": "ap_nanjing"
 }""")
     logger.error(
-        'config.json 未创建，程序已自动创建，请参考 https://config.ymbot.top 填写该文件的内容')
+        'config.json 未创建，程序已自动创建')
     sys.exit(1)
+
+
+def get_config(name: str):
+    try:
+        return config_yaml[name]
+    except KeyError:
+        logger.error(f'{name} 在配置文件中找不到')
+        return None
+
+
+def get_cloud_config(name: str):
+    try:
+        return cloud_config_json[name]
+    except KeyError:
+        logger.error(f'{name} 在配置文件中找不到')
+        return ""
--- main.py
+++ main.py
@@ -1,38 +1,242 @@
-from botpy import logger
-from botpy.message import Message
+from botpy import logging
 
-import botpy
-import json
-import pathlib
-import os
-import yaml
+import botfunc
+import pymysql
 
-curPath = os.path.dirname(os.path.realpath(__file__))
-# 查找yaml配置文件
-config_yaml = yaml.safe_load(open('../YanBot_infp/config.yaml', 'r', encoding='UTF-8'))
+if botfunc.get_config('enable_mysql'):
+    try:
+        conn = pymysql.connect(host=botfunc.get_cloud_config('MySQL_Host'), port=botfunc.get_cloud_config('MySQL_Port'),
+                               user=botfunc.get_cloud_config('MySQL_User'),
+                              password=botfunc.get_cloud_config('MySQL_Pwd'), charset='utf8mb4',
+                               database=botfunc.get_cloud_config('MySQL_db'))
+        cursor = conn.cursor()
+    except pymysql.err.InternalError:
+        conn = pymysql.connect(host=botfunc.get_cloud_config('MySQL_Host'), port=botfunc.get_cloud_config('MySQL_Port'),
+                               user=botfunc.get_cloud_config('MySQL_User'),
+                               password=botfunc.get_cloud_config('MySQL_Pwd'), charset='utf8mb4')
+        cursor = conn.cursor()
+        cursor.execute("""create database if not exists %s""", (botfunc.get_cloud_config('MySQL_db'),))
 
-# 查找成功之后，赋值
-appid = (type("bot_app_id"))
-token = (type("bot_app_token"))
+    cursor.execute("""create table if not exists admin
+    (
+        uid bigint unsigned default '0' not null
+            primary key
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
 
-# 读取json文件
-channel = json.loads(pathlib.Path("./json/chennel").read_text("mychannel"))
-channelid = json.loads(pathlib.Path("./json/chennel").read_text("boxid"))
+    cursor.execute("""create table if not exists sblist
+    (
+        uid bigint unsigned default '0' not null
+            primary key
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
 
-# 显示原Channel消息
-logger.info("Bot将会运行在",channel,"频道上")
-logger.warning("Bot将会运行在",channelid,"沙箱ID上")
+    cursor.execute("""create table if not exists blacklist
+    (
+        uid bigint unsigned not null
+            primary key,
+        op  bigint unsigned not null
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
 
-# 测试消息，发送官方文档
-class MyClient(botpy.Client):
-    async def on_at_message_create(self, message: Message):
-        await message.reply(content=f"机器人的官方文档:https://bot.ymbot.top")
+    cursor.execute("""create table if not exists bread
+    (
+        id         int unsigned auto_increment
+            primary key,
+        level      int unsigned default '0' not null,
+        time       int unsigned default '0' not null,
+        bread      int unsigned default '0' not null,
+        experience int unsigned default '0' not null
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
 
-# 激活
-intents = botpy.Intents(public_guild_messages=True)
-client = MyClient(intents=intents)
+    cursor.execute("""create table if not exists wd
+    (
+        wd    tinytext     null,
+        count int unsigned null
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
 
-# 连接腾讯QQ频道平台
-client.run(appid=appid, token=token)
+    cursor.execute("""create table if not exists woodenfish
+    (
+        uid       bigint unsigned          not null comment '赛博（QQ）账号'
+            primary key,
+        time      bigint unsigned          not null comment '上次计算时间',
+        level     int unsigned default '0' not null comment '木鱼等级',
+        de        bigint       default 0   not null comment '功德',
+        e         double       default 0   not null comment 'log10值',
+        ee        double       default 0   not null comment 'log10^10值',
+        nirvana   double       default 1   not null comment '涅槃重生次数',
+        ban       int          default 0   not null comment '封禁状态',
+        dt        bigint       default 0   not null comment '封禁结束时间',
+        end_time  bigint       default 0   not null comment '最近一次调用时间',
+        hit_count int          default 0   not null comment '一周期内的调用次数'
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `six` ( 
+    `uid` bigint UNSIGNED NOT NULL PRIMARY KEY COMMENT 'QQ号' ,
+    `count` int UNSIGNED NOT NULL DEFAULT 0 COMMENT '6 的次数',
+    `ti` bigint UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后一次"6"发送时间'
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `no_six` ( 
+    `gid` bigint UNSIGNED NOT NULL PRIMARY KEY COMMENT '群号'
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `inm` ( 
+    `gid` bigint UNSIGNED NOT NULL PRIMARY KEY COMMENT '群号'
+    ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
 
-# 导入modules文件夹
+    conn.commit()
+
+    cursor.execute('SELECT wd, count FROM wd')
+    cache_var.sensitive_words = [x[0] for x in cursor.fetchall()]
+    if not cache_var.sensitive_words:
+        print("未找到敏感词库！即将从GitHub仓库拉取……（请保证能正常访问jsDelivr）")
+        input("> 是否继续？（回车 继续 / ^C 退出）")
+        # 色情类
+        d = requests.get(
+            "https://cdn.jsdelivr.net/gh/fwwdn/sensitive-stop-words@master/%E8%89%B2%E6%83%85%E7%B1%BB.txt").text.split(
+            ',\n')
+        # 政治类
+        d.extend(
+            requests.get(
+                "https://cdn.jsdelivr.net/gh/fwwdn/sensitive-stop-words@master/%E6%94%BF%E6%B2%BB%E7%B1%BB.txt"
+            ).text.split(',\n')
+        )
+        # 违法类
+        d.extend(
+            requests.get(
+                "https://cdn.jsdelivr.net/gh/fwwdn/sensitive-stop-words@master/%E6%B6%89%E6%9E%AA%E6%B6%89%E7%88%86%E8%BF"
+                "%9D%E6%B3%95%E4%BF%A1%E6%81%AF%E5%85%B3%E9%94%AE%E8%AF%8D.txt"
+            ).text.split(',\n')
+        )
+        d.pop(-1)  # 上面的这些加载出来在列表末尾会多出一堆乱码，故删除，如果你需要魔改此部分请视情况自行删除
+        for w in track(d, description="Loading"):
+            cursor.execute("INSERT INTO wd VALUES (%s, 0)", (w,))
+            try:
+                conn.commit()
+            except pymysql.err.DataError:
+                conn.rollback()
+    cursor.execute('SELECT wd, count FROM wd')
+    cache_var.sensitive_words = [x[0] for x in cursor.fetchall()]
+
+    cursor.execute('SELECT gid FROM no_six')
+    cache_var.no_6 = [x[0] for x in cursor.fetchall()]
+
+    cursor.execute('SELECT uid FROM admin')
+    if not cursor.fetchall():
+        admin_uid = int(input("未找到任何一个op！请输入你（op）的QQ号："))
+        cursor.execute("INSERT INTO admin VALUES (%s)", (admin_uid,))
+
+    conn.commit()
+
+    cursor.execute('SELECT gid FROM inm')
+    cache_var.inm = [x[0] for x in cursor.fetchall()]
+
+    conn.close()
+else:
+        conn = sqlite3.connect('database.db'),
+        cursor = conn.cursor()
+    cursor.execute("""create table if not exists admin
+            (
+                uid bigint unsigned default '0' not null
+                    primary key
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+
+    cursor.execute("""create table if not exists sblist
+            (
+                uid bigint unsigned default '0' not null
+                    primary key
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+
+    cursor.execute("""create table if not exists blacklist
+            (
+                uid bigint unsigned not null
+                    primary key,
+                op  bigint unsigned not null
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+
+    cursor.execute("""create table if not exists bread
+            (
+                id         int unsigned auto_increment
+                    primary key,
+                level      int unsigned default '0' not null,
+                time       int unsigned default '0' not null,
+                bread      int unsigned default '0' not null,
+                experience int unsigned default '0' not null
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+
+    cursor.execute("""create table if not exists wd
+            (
+                wd    tinytext     null,
+                count int unsigned null
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+
+    cursor.execute("""create table if not exists woodenfish
+            (
+                uid       bigint unsigned          not null comment '赛博（QQ）账号'
+                    primary key,
+                time      bigint unsigned          not null comment '上次计算时间',
+                level     int unsigned default '0' not null comment '木鱼等级',
+                de        bigint       default 0   not null comment '功德',
+                e         double       default 0   not null comment 'log10值',
+                ee        double       default 0   not null comment 'log10^10值',
+                nirvana   double       default 1   not null comment '涅槃重生次数',
+                ban       int          default 0   not null comment '封禁状态',
+                dt        bigint       default 0   not null comment '封禁结束时间',
+                end_time  bigint       default 0   not null comment '最近一次调用时间',
+                hit_count int          default 0   not null comment '一周期内的调用次数'
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `six` ( 
+            `uid` bigint UNSIGNED NOT NULL PRIMARY KEY COMMENT 'QQ号' ,
+            `count` int UNSIGNED NOT NULL DEFAULT 0 COMMENT '6 的次数',
+            `ti` bigint UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后一次"6"发送时间'
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `no_six` ( 
+            `gid` bigint UNSIGNED NOT NULL PRIMARY KEY COMMENT '群号'
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `inm` ( 
+            `gid` bigint UNSIGNED NOT NULL PRIMARY KEY COMMENT '群号'
+            ) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_general_ci" """)
+
+    conn.commit()
+
+    cursor.execute('SELECT wd, count FROM wd')
+    cache_var.sensitive_words = [x[0] for x in cursor.fetchall()]
+    if not cache_var.sensitive_words:
+        print("未找到敏感词库！即将从GitHub仓库拉取……（请保证能正常访问jsDelivr）")
+        input("> 是否继续？（回车 继续 / ^C 退出）")
+        # 色情类
+        d = requests.get(
+            "https://cdn.jsdelivr.net/gh/fwwdn/sensitive-stop-words@master/%E8%89%B2%E6%83%85%E7%B1%BB.txt").text.split(
+            ',\n')
+        # 政治类
+        d.extend(
+            requests.get(
+                "https://cdn.jsdelivr.net/gh/fwwdn/sensitive-stop-words@master/%E6%94%BF%E6%B2%BB%E7%B1%BB.txt"
+            ).text.split(',\n')
+        )
+        # 违法类
+        d.extend(
+            requests.get(
+                "https://cdn.jsdelivr.net/gh/fwwdn/sensitive-stop-words@master/%E6%B6%89%E6%9E%AA%E6%B6%89%E7%88%86%E8%BF"
+                "%9D%E6%B3%95%E4%BF%A1%E6%81%AF%E5%85%B3%E9%94%AE%E8%AF%8D.txt"
+            ).text.split(',\n')
+        )
+        d.pop(-1)  # 上面的这些加载出来在列表末尾会多出一堆乱码，故删除，如果你需要魔改此部分请视情况自行删除
+        for w in track(d, description="Loading"):
+            cursor.execute("INSERT INTO wd VALUES (%s, 0)", (w,))
+            try:
+                conn.commit()
+            except pymysql.err.DataError:
+                conn.rollback()
+    cursor.execute('SELECT wd, count FROM wd')
+    cache_var.sensitive_words = [x[0] for x in cursor.fetchall()]
+
+    cursor.execute('SELECT gid FROM no_six')
+    cache_var.no_6 = [x[0] for x in cursor.fetchall()]
+
+    cursor.execute('SELECT uid FROM admin')
+    if not cursor.fetchall():
+        admin_uid = int(input("未找到任何一个op！请输入你（op）的QQ号："))
+        cursor.execute("INSERT INTO admin VALUES (%s)", (admin_uid,))
+
+    conn.commit()
+
+    cursor.execute('SELECT gid FROM inm')
+    cache_var.inm = [x[0] for x in cursor.fetchall()]
+
+    conn.close()
